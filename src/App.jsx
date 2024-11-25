48c114c577161a1b9b3af164d737af1fb73aedb1
Andrii <88sushilnikov@gmail.com>
2024-11-25 22:27:31 +0200

    task-2-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-import userData from './userData.json'
 import Profile from './components/Profile/Profile'
+import FriendList from './components/FriendList/FriendList'
+import TransactionHistory from './components/TransactionHistory/TransactionHistory'
+import userData from './userData.json'
+import friends from './friends.json'
+import transactions from './transactions.json'
 import './App.css'
 
 const App = () => {
@@ -12,6 +16,8 @@ const App = () => {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
