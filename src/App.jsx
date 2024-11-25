3089fcbf0e3d6b2e35634909b7a223c01013943d
Andrii <88sushilnikov@gmail.com>
2024-11-25 02:42:30 +0200

    task-1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import userData from './userData.json'
+import Profile from './components/Profile/Profile'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
+
+export default App
